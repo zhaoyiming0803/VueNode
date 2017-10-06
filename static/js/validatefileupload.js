342018dda044f8f4478b0806e3fcae2456eed2d1
zym <1047832475@qq.com>
2017-10-06 15:59:16 +0800

    调整代码格式

--- static/js/validatefileupload.js
+++ static/js/validatefileupload.js
@@ -6,15 +6,9 @@
 */
 
 ;(function (global, oDoc, factory) {
-
-	typeof exports === 'object' && typeof module !== 'undefined'
-	? module.exports = factory(oDoc)
-	: typeof define === 'function' && define.amd
-	? define([], function () {
-		return factory(oDoc);
-	})
-	: (global.validateFileUpload = factory(oDoc));
-
+	typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory(oDoc) :
+	typeof define === 'function' && define.amd ? define([], function () {return factory(oDoc);}) :
+	(global.validateFileUpload = factory(oDoc));
 })(this, document, function (oDoc) {
 	'use strict';
 
