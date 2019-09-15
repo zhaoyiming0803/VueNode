ec24cc7dfe47ac33aed9c7b9ac3f25dfd304ad9e
zymfe <1047832475@qq.com>
2019-09-15 21:33:51 +0800

    chore: format codes

--- fe/build/webpack.base.config.js
+++ fe/build/webpack.base.config.js
@@ -11,62 +11,56 @@ const resolve = dir => path.resolve(__dirname, '../', dir);
 
 module.exports = config => {
 	config.resolve.alias
-    .set('public', resolve('public'))
-    .set('@', resolve('src'));
-  config.resolve.extensions
-    .add('.less')
-    .add('.css');
+		.set('public', resolve('public'))
+		.set('@', resolve('src'));
+	config.resolve.extensions
+		.add('.less')
+		.add('.css');
 
-  config.plugins
-    .delete('prefetch')
-    .delete('preload');
+	config.plugins
+		.delete('prefetch')
+		.delete('preload');
 
-  config.plugin('inline-source')
-    .use(require('html-webpack-inline-source-plugin'))
-    .after('html');
+	config.plugin('inline-source')
+		.use(require('html-webpack-inline-source-plugin'))
+		.after('html');
 
-  config.plugin('script-ext')
-    .use(require('script-ext-html-webpack-plugin'), [{
-      defaultAttribute: 'defer'
-    }])
-    .after('html');
+	config.plugin('script-ext')
+		.use(require('script-ext-html-webpack-plugin'), [{
+			defaultAttribute: 'defer'
+		}])
+		.after('html');
 
-  config.plugin('html')
-    .tap(args => {
-      args[0].inlineSource = '.(app|chunk-vendors).*.(css)';
-      args[0].minify = undefined;
-      args[0].var = {
-        NODE_ENV: process.env.NODE_ENV
-      }
-      return args;
-    });
-
-	config.module
-		.rule('ts')
-		.use('ts-loader')
-		.tap(options => {
-			options = merge(options, {
-				transpileOnly: true,
-				getCustomTransformers: () => ({
-					before: [
-						tsImportPluginFactory({
-							libraryName: 'vant',
-							libraryDirectory: 'es',
-							style: true
-						})
-					]
-				}),
-				compilerOptions: {
-					module: 'es2015'
-				}
-			});
-			return options;
-		});
-	config
-		.plugin('copy')
+	config.plugin('html')
 		.tap(args => {
-			args[0][0].from = resolve('src/static/uploads');
-			args[0][0].to = 'static/uploads'
+			args[0].inlineSource = '.(app|chunk-vendors).*.(css)';
+			args[0].minify = undefined;
+			args[0].var = {
+				NODE_ENV: process.env.NODE_ENV
+			}
 			return args;
 		});
+
+	config.module.rule('ts').use('ts-loader').tap(options = > {
+		options = merge(options, {
+			transpileOnly: true,
+			getCustomTransformers: () = > ({
+				before: [tsImportPluginFactory({
+					libraryName: 'vant',
+					libraryDirectory: 'es',
+					style: true
+				})]
+			}),
+			compilerOptions: {
+				module: 'es2015'
+			}
+		});
+		return options;
+	});
+
+	config.plugin('copy').tap(args = > {
+		args[0][0].from = resolve('src/static/uploads');
+		args[0][0].to = 'static/uploads'
+			return args;
+	});
 }
