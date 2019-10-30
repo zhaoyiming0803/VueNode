0753ce1c170fb4527816a3c158d719abb9671c9d
zymfe <1047832475@qq.com>
2019-10-30 18:12:39 +0800

    fea: get location

--- fe/src/libs/wx/index.ts
+++ fe/src/libs/wx/index.ts
@@ -29,7 +29,8 @@ async function wechat() {
       'chooseImage',
       'uploadImage',
       'downloadImage',
-      'scanQRCode'
+      'scanQRCode',
+      'getLocation'
     ]
   });
 
@@ -56,6 +57,13 @@ async function wechat() {
 
     wx.onMenuShareTimeline(shareOption);
     wx.onMenuShareAppMessage(shareOption);
+    wx.getLocation({
+      type: 'wgs84', // 默认为wgs84的gps坐标，如果要返回直接给openLocation用的火星坐标，可传入'gcj02'
+      success: function (res) {
+        var latitude = res.latitude; // 纬度，浮点数，范围为90 ~ -90
+        var longitude = res.longitude; // 经度，浮点数，范围为180 ~ -180。
+      }
+    });
   });
 }
 
