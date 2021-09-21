0d52f5970cf858f1933c8660ff74a382f8c61e96
zhaoyiming0803 <zhaoyiming0803@gmail.com>
2021-09-21 09:24:39 +0800

    chore: format codes

--- fe/build/webpack.base.config.js
+++ fe/build/webpack.base.config.js
@@ -3,43 +3,43 @@
  * @author zhaoyiming
  * @since  2019/09/14
  */
-const merge = require('webpack-merge');
-const tsImportPluginFactory = require('ts-import-plugin');
+const merge = require('webpack-merge')
+const tsImportPluginFactory = require('ts-import-plugin')
 
-const path = require('path');
-const resolve = dir => path.resolve(__dirname, '../', dir);
+const path = require('path')
+const resolve = dir => path.resolve(__dirname, '../', dir)
 
 module.exports = config => {
 	config.resolve.alias
 		.set('public', resolve('public'))
-		.set('@', resolve('src'));
+		.set('@', resolve('src'))
 	config.resolve.extensions
 		.add('.less')
-		.add('.css');
+		.add('.css')
 
 	config.plugins
 		.delete('prefetch')
-		.delete('preload');
+		.delete('preload')
 
 	config.plugin('inline-source')
 		.use(require('html-webpack-inline-source-plugin'))
-		.after('html');
+		.after('html')
 
 	config.plugin('script-ext')
 		.use(require('script-ext-html-webpack-plugin'), [{
 			defaultAttribute: 'defer'
 		}])
-		.after('html');
+		.after('html')
 
 	config.plugin('html')
 		.tap(args => {
-			args[0].inlineSource = '.(app|chunk-vendors).*.(css)';
-			args[0].minify = undefined;
+			args[0].inlineSource = '.(app|chunk-vendors).*.(css)'
+			args[0].minify = undefined
 			args[0].var = {
 				NODE_ENV: process.env.NODE_ENV
 			}
-			return args;
-		});
+			return args
+		})
 
 	config.module.rule('ts').use('ts-loader').tap(options => {
 		options = merge(options, {
@@ -54,13 +54,13 @@ module.exports = config => {
 			compilerOptions: {
 				module: 'es2015'
 			}
-		});
-		return options;
-	});
+		})
+		return options
+	})
 
 	config.plugin('copy').tap(args => {
-		args[0][0].from = resolve('src/static');
+		args[0][0].from = resolve('src/static')
 		args[0][0].to = 'static'
-			return args;
-	});
+			return args
+	})
 }
--- server/index.js
+++ server/index.js
@@ -1,26 +1,26 @@
-const express = require('express');
-const app = express();
-const bodyParser = require('body-parser');
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Access-Control-Expose-Headers, Platform, Token, Uid');
-  res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS, HEAD');
-  res.header('Content-Type', 'application/json; charset=utf-8');
-  next();
-});
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Access-Control-Expose-Headers, Platform, Token, Uid')
+  res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS, HEAD')
+  res.header('Content-Type', 'application/json charset=utf-8')
+  next()
+})
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
-app.use('/tour/auth', require(__dirname + '/module/auth'));
-app.use('/tour/user', require(__dirname + '/module/user'));
-app.use('/tour/coupon', require(__dirname + '/module/coupon'));
-app.use('/tour/region', require(__dirname + '/module/region'));
-app.use('/tour/banner', require(__dirname + '/module/banner'));
-app.use('/tour/article', require(__dirname + '/module/article'));
-app.use('/tour/comment', require(__dirname + '/module/comment'));
+app.use('/tour/auth', require(__dirname + '/module/auth'))
+app.use('/tour/user', require(__dirname + '/module/user'))
+app.use('/tour/coupon', require(__dirname + '/module/coupon'))
+app.use('/tour/region', require(__dirname + '/module/region'))
+app.use('/tour/banner', require(__dirname + '/module/banner'))
+app.use('/tour/article', require(__dirname + '/module/article'))
+app.use('/tour/comment', require(__dirname + '/module/comment'))
 
 app.listen(8091, () => {
-  console.log('express listen port 8091');
-});
+  console.log('express listen port 8091')
+})
